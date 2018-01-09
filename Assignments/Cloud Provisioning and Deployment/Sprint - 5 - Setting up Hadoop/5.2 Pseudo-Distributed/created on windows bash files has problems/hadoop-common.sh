61e04ca6ccd7fce6c2685381c3b8da9cc0ed9503
worknehy1 <aoay17@student.bth.se>
2018-01-09 21:22:19 +0100

    hadoop
    hadoop with error

---
+++ Assignments/Cloud Provisioning and Deployment/Sprint - 5 - Setting up Hadoop/5.2 Pseudo-Distributed/created on windows bash files has problems/hadoop-common.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export HADOOP_VER=hadoop-2.8.1
+export HADOOP_ROOT_ROOT=/usr/local
+export HADOOP_ROOT=$HADOOP_ROOT_ROOT/hadoop
+export HADOOP_HOME=$HADOOP_ROOT
+export HADOOP_PREFIX=$HADOOP_HOME
+
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin 
+
+# Set further environment variables (Strictly not necessary yet, but will be for Pseudo-mode)
+export HADOOP_MAPRED_HOME=$HADOOP_HOME 
+export HADOOP_COMMON_HOME=$HADOOP_HOME 
+export HADOOP_HDFS_HOME=$HADOOP_HOME 
+export YARN_HOME=$HADOOP_HOME 
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
+export HADOOP_INSTALL=$HADOOP_HOME
+
+export HADOOP_LOG_DIR=/tmp/hadoop-logs
